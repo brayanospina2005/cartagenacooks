e38e7852168bc8b5ef98a714d2b301fb3fb8af5a
brayanospina2005 <ospinabrayan431@gmail.com>
2022-07-14 19:12:17 -0500

    loader terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
+/* scrips of the sidebar */
 let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
+for (let i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener("click", (e)=>{
  let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
  arrowParent.classList.toggle("showMenu");
@@ -11,3 +12,13 @@ console.log(sidebarBtn);
 sidebarBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("close");
 });
+
+/* finish sidebar */
+
+/* init loader  */
+window.onload = function() {
+  let contenedor = document.getElementById('contenedor_carga');
+  contenedor.style.visibility = 'hidden';
+  contenedor.style.opacity = '0';
+}
+/* finish loader */
